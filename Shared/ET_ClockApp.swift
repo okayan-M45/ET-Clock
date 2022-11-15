d3a4a58657689a2282cffbcc2df763ee96904e62
岡田 彩実 <okada.a.2907@gmail.com>
2022-11-15 11:24:51 +0900

    Initial Commit

---
+++ Shared/ET_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ET_ClockApp.swift
+//  Shared
+//
+//  Created by USER on 2022/11/15.
+//
+
+import SwiftUI
+
+@main
+struct ET_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
